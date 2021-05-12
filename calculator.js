2bde00841e23ee4151e63185258036057e287cdd
danielmarin05 <marinx2010@gmail.com>
2021-05-12 15:01:12 -0500

    Primer commit, HTML y CSS completos, iniciando JavaScript

---
+++ calculator.js
@@ -0,0 +1,36 @@
+function init(){
+    let resultado = document.getElementById('resultado');
+    let suma = document.getElementById('suma');
+    let resta = document.getElementById('resta');
+    let multi = document.getElementById('multi');
+    let div = document.getElementById('div');
+    let result = document.getElementById('result');
+    let n1 = document.getElementById('n1');
+    let n2 = document.getElementById('n2');
+    let n3 = document.getElementById('n3');
+    let n4 = document.getElementById('n4');
+    let n5 = document.getElementById('n5');
+    let n6 = document.getElementById('n6');
+    let n7 = document.getElementById('n7');
+    let n8 = document.getElementById('n8');
+    let n9 = document.getElementById('n9');
+    let n0 = document.getElementById('n0');
+    let clean = document.getElementById('clean');
+}
+
+
+
+function resultado (){
+    switch (opera){
+        case '+':
+            total= total + auxiliar;
+            console.log(total);
+            break;
+        case '-':
+        case '*':
+        case '/':
+    }
+}
+
+
+
