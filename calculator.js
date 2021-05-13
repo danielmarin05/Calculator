844f32d067c157a303cb191eceff23a54dd16b40
danielmarin05 <marinx2010@gmail.com>
2021-05-13 08:56:17 -0500

    Responsive agregado y funcionando correctamente, botón eliminar funcionando correctamente

--- calculator.js
+++ calculator.js
@@ -1,28 +1,56 @@
 var pantalla = document.getElementById('resultadoP');
-
-
+var auxiliar = 0;
+let operador =null;
+let total = 0;
 
 function numero (num){
-    console.log(num);
     pantalla.innerText += num.toString();
+    console.log(num);
 }
 
+function operacion (signo){
+    console.log(signo);
+        operador = signo;
+        auxiliar = pantalla.innerText;
+        operar();
+        pantalla.innerText = '';
+}
 
-function resultado (){
-    switch (opera){
+function operar (){
+    
+    switch (operador){
         case '+':
+            total = parseFloat(auxiliar) + parseFloat(pantalla.innerText);
+            pantalla.innerText = total.toString();
             console.log(total);
             break;
         case '-':
+            console.log(pantalla.innerText);
+            total = parseFloat(auxiliar) - parseFloat(pantalla.innerText);
+            pantalla.innerText = total.toString();
             console.log(total);
             break;
         case '*':
+            console.log(pantalla.innerText);
+            total = parseFloat(auxiliar) * parseFloat(pantalla.innerText);
+            pantalla.innerText = total.toString();
             console.log(total);
             break;
         case '/':
+            console.log(pantalla.innerText);
+            total = parseFloat(auxiliar) / parseFloat(pantalla.innerText);
+            pantalla.innerText = total.toString();
             console.log(total);
             break;
+
+            case 'c':
+                pantalla.innerText = '';
+                auxiliar = 0;
+                perador =null;
+                total = 0;
+                break;
     }
+    
 }
 
 
